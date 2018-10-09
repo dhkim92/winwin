249a70d98a7dac22904cf4d5a093dfc46ad22f2e
Youngeun <Youngeun@DESKTOP-DBOO8T6>
2018-10-09 16:44:34 +0900

    영은

---
+++ Win-win/src/main/java/winwin/YEController.java
@@ -0,0 +1,14 @@
+package winwin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class YEController {
+	
+	@RequestMapping(value="/ye/ye")
+	public void ye() {
+		
+	}
+
+}
