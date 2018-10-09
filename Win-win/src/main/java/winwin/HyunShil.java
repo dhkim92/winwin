918b405b8bbff7d371ff525f24306c9bd7f3c14e
god27 <god27@DESKTOP-0DLUSF5>
2018-10-09 16:46:17 +0900

    181009 현실

---
+++ Win-win/src/main/java/winwin/HyunShil.java
@@ -0,0 +1,13 @@
+package winwin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HyunShil {
+
+	@RequestMapping(value="/hyunshil")
+	public void Test() {
+		
+	}
+}
