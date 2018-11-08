2bf07480889eaaa1d675ce58dd20b0e0f976dc6a
User <User@DESKTOP-TA186O2>
2018-11-08 14:08:55 +0900

    181108 seungri jobopenbasic dto update

--- Win-win/src/main/java/winwin/dto/JobopenBasic.java
+++ Win-win/src/main/java/winwin/dto/JobopenBasic.java
@@ -26,17 +26,33 @@ public class JobopenBasic {
 	private Date writeDate; // 공고 작성날짜
 	private int hit; // 조회수
 	
-	
-	
+	private int applicantCnt; // 지원자 수 
+
 	@Override
 	public String toString() {
 		return "JobopenBasic [jobopenNo=" + jobopenNo + ", title=" + title + ", offer=" + offer + ", form=" + form
 				+ ", startPay=" + startPay + ", endPay=" + endPay + ", rulePay=" + rulePay + ", startDate=" + startDate
 				+ ", endDate=" + endDate + ", allOpen=" + allOpen + ", academic=" + academic + ", score=" + score
 				+ ", lang=" + lang + ", etcQual=" + etcQual + ", etc=" + etc + ", writeDate=" + writeDate + ", hit="
-				+ hit + "]";
+				+ hit + ", applicantCnt=" + applicantCnt + "]";
 	}
-	
+
+	public int getJobopenNo() {
+		return jobopenNo;
+	}
+
+	public void setJobopenNo(int jobopenNo) {
+		this.jobopenNo = jobopenNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
 	public String getOffer() {
 		return offer;
 	}
@@ -44,105 +60,127 @@ public class JobopenBasic {
 	public void setOffer(String offer) {
 		this.offer = offer;
 	}
-	
-	public String getTitle() {
-		return title;
-	}
 
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	public Date getWriteDate() {
-		return writeDate;
-	}
-	public void setWriteDate(Date writeDate) {
-		this.writeDate = writeDate;
-	}
-	public int getHit() {
-		return hit;
-	}
-	public void setHit(int hit) {
-		this.hit = hit;
-	}
-	
-	public int getJobopenNo() {
-		return jobopenNo;
-	}
-	public void setJobopenNo(int jobopenNo) {
-		this.jobopenNo = jobopenNo;
-	}
 	public String getForm() {
 		return form;
 	}
+
 	public void setForm(String form) {
 		this.form = form;
 	}
+
 	public int getStartPay() {
 		return startPay;
 	}
+
 	public void setStartPay(int startPay) {
 		this.startPay = startPay;
 	}
+
 	public int getEndPay() {
 		return endPay;
 	}
+
 	public void setEndPay(int endPay) {
 		this.endPay = endPay;
 	}
+
 	public String getRulePay() {
 		return rulePay;
 	}
+
 	public void setRulePay(String rulePay) {
 		this.rulePay = rulePay;
 	}
+
 	public Date getStartDate() {
 		return startDate;
 	}
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+
 	public Date getEndDate() {
 		return endDate;
 	}
+
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
+
 	public String getAllOpen() {
 		return allOpen;
 	}
+
 	public void setAllOpen(String allOpen) {
 		this.allOpen = allOpen;
 	}
+
 	public String getAcademic() {
 		return academic;
 	}
+
 	public void setAcademic(String academic) {
 		this.academic = academic;
 	}
+
 	public String getScore() {
 		return score;
 	}
+
 	public void setScore(String score) {
 		this.score = score;
 	}
+
 	public String getLang() {
 		return lang;
 	}
+
 	public void setLang(String lang) {
 		this.lang = lang;
 	}
+
 	public String getEtcQual() {
 		return etcQual;
 	}
+
 	public void setEtcQual(String etcQual) {
 		this.etcQual = etcQual;
 	}
+
 	public String getEtc() {
 		return etc;
 	}
+
 	public void setEtc(String etc) {
 		this.etc = etc;
 	}
+
+	public Date getWriteDate() {
+		return writeDate;
+	}
+
+	public void setWriteDate(Date writeDate) {
+		this.writeDate = writeDate;
+	}
+
+	public int getHit() {
+		return hit;
+	}
+
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+
+	public int getApplicantCnt() {
+		return applicantCnt;
+	}
+
+	public void setApplicantCnt(int applicantCnt) {
+		this.applicantCnt = applicantCnt;
+	}
+	
+	
+
 }
